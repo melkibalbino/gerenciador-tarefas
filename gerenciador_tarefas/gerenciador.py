bd5524d83cdb8d3977da8b2a632c1ae59c11c6d0
melkibalbino <melkibalbinoads@gmail.com>
2021-08-18 10:26:15 -0300

    adicionando recurso de listar tarefas

---
+++ gerenciador_tarefas/gerenciador.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+TAREFAS = [
+    {
+        "id": "1",
+        "titulo": "fazer compras",
+        "descrição": "comprar leite e ovos",
+        "estado": "não finalizado",
+    },
+    {
+        "id": "2",
+        "titulo": "levar o cachorro para tosar",
+        "descrição": "está muito peludo",
+        "estado": "não finalizado",
+    },
+    {
+        "id": "3",
+        "titulo": "lavar roupas",
+        "descrição": "estão sujas",
+        "estado": "não finalizado",
+    },
+]
+
+
+@app.get('/tarefas')
+def listar():
+    return TAREFAS
